e2db669ffc73fc0ddc48c85082d0dd6a59582a6b
hirancph <hiranchandph@gmail.com>
2026-09-14 00:07:34 +0530

    fix(kitty): remove extra transparency in kitty

--- home/gui/terminal/kitty/default.nix
+++ home/gui/terminal/kitty/default.nix
@@ -10,7 +10,6 @@
 
     settings = {
       hide_window_decorations = "titlebar-and-corners";
-      background_opacity = lib.mkForce "0.8";
       window_padding_width = 10;
       placement_strategy = "center";
 
