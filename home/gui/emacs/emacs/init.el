0ff023609100d8bd73a26d4e69d9baf0a970da2c
hirancph <hiranchandph@gmail.com>
2026-09-16 23:13:15 +0530

    feat(emacs): add modern companians for vertico

--- home/gui/emacs/emacs/init.el
+++ home/gui/emacs/emacs/init.el
@@ -50,12 +50,45 @@
   :init
   (vertico-mode))
 
+;; Marginalia adds useful information for completions, like docstrings, keybinds, etc...
+(use-package marginalia
+  :ensure t
+  :init
+  (marginalia-mode))
+
+;; Orderless enables better searching
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+;; Consult gives live previews for Search and buffer navigation
+(use-package consult
+  :ensure t
+  :bind (;; Replacement for standard commands
+	 ("C-x b" . consult-buffer))) ; switch buffer with live previews
+
+;; Corfu gives the floating auto-completion at cursor
+(use-package corfu
+  :ensure t
+  :init
+  (global-corfu-mode)
+  :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline doom-themes nano-modeline vertico)))
+ '(package-selected-packages
+   '(consult corfu doom-modeline doom-themes marginalia nano-modeline
+	     orderless vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
