a3f243e63265ee10436604a4b0af146a32d4089f
hirancph <hiranchandph@gmail.com>
2026-09-16 20:21:32 +0530

    feat(emacs): add vertico

--- home/gui/emacs/emacs/init.el
+++ home/gui/emacs/emacs/init.el
@@ -44,12 +44,18 @@
   (nano-modeline-text-mode t)
   (nano-modeline-prog-mode t))
 
+;; Verical completion UI for things like M-x C-x b
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline doom-themes nano-modeline)))
+ '(package-selected-packages '(doom-modeline doom-themes nano-modeline vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
