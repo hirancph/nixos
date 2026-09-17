9c95b96c490ea6f014bfc795da470a840e836cd7
hirancph <hiranchandph@gmail.com>
2026-09-17 10:41:39 +0530

    feat(emacs): add straight.el

--- home/gui/emacs/emacs/init.el
+++ home/gui/emacs/emacs/init.el
@@ -1,5 +1,22 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Install straight.el if not present
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p user-emacs-directory)
+            "~/.emacs.d/")))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
