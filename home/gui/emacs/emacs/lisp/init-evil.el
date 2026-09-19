0fd70ae55bc64a8b3b30cc63a700b072bf43a59b
hirancph <hiranchandph@gmail.com>
2026-09-19 16:32:21 +0530

    feat(emacs): change cursor for normal mode in terminal

--- home/gui/emacs/emacs/lisp/init-evil.el
+++ home/gui/emacs/emacs/lisp/init-evil.el
@@ -9,6 +9,9 @@
   :init
   (setq evil-undo-system 'undo-fu)
   (setq evil-want-keybinding nil) ; Required by evil-collection
+  (setq evil-normal-state-cursor 'box
+        evil-insert-state-cursor 'bar
+        evil-visual-state-cursor 'hollow)
   :config
   (evil-mode 1))
 
@@ -17,5 +20,13 @@
   :ensure t
   :config
   (evil-collection-init))
+
+;; Enable cursor shape changes specifically for terminal Emacs
+;; Enable terminal cursor shape changes for Evil mode in -nw
+(unless (display-graphic-p)
+  (use-package evil-terminal-cursor-changer
+    :ensure t
+    :config
+    (etcc-on)))
 
 (provide 'init-evil)
