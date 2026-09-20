fa37c5c06317ef4ad79346f358eb74efb1263e73
hirancph <hiranchandph@gmail.com>
2026-09-20 12:24:41 +0530

    feat(emacs): reflect disk changes

--- home/gui/emacs/emacs/early-init.el
+++ home/gui/emacs/emacs/early-init.el
@@ -4,3 +4,12 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+;; global-auto-revert-mode reloads file-visiting buffers
+(global-auto-revert-mode 1)
+
+;; Automatically refresh Dired and other non-file buffers when files change on disk
+(setq global-auto-revert-non-file-buffers t)
+
+;; Silence "Reverting buffer..." messages in the echo area
+(setq auto-revert-verbose nil)
