e5b42fcca909f94d1cf6b70b0cfbef57f3369c8b
hirancph <hiranchandph@gmail.com>
2026-09-21 22:08:48 +0530

    feat(emacs): setup org like nano-emacs

--- home/gui/emacs/emacs/lisp/init-org.el
+++ home/gui/emacs/emacs/lisp/init-org.el
@@ -24,13 +24,44 @@
  'org-babel-load-languages
  '((emacs-lisp . t)
    (python . t)))
-;; Hide leading stars and change bullet points
-(use-package org-superstar
-  :ensure t
-  :hook (org-mode . org-superstar-mode)
+
+;; Org buffer clean-up settings
+(use-package org
   :config
-  (setq org-superstar-headline-bullets-list '(""))
-  (setq org-superstar-leading-bullet ""))
+  ;; Hide keyword tags (#+TITLE, #+AUTHOR, etc.)
+  (setq org-hidden-keywords '(title author date startup options))
+  ;; Cleanly hide leading stars without overlay/ghost-cursor bugs
+  (setq org-hide-leading-stars t)
+  ;; Subtle line spacing for better reading flow
+  (setq-default line-spacing 0.15))
+
+;; Hide leading stars and change bullet points
+(setq org-hide-leading-stars t)
+
+;; NΛNO-style heading numbering
+(use-package org-num
+  :after org
+  :hook (org-mode . org-num-mode)
+  :config
+  (setq org-num-skip-unnumbered t)
+  (setq org-num-max-level 3)
+  (setq org-num-format-function
+      (lambda (numbering)
+        (let ((num-str (mapconcat #'number-to-string numbering ".")))
+          (if (= (length numbering) 1)
+              ;; Level 1 Heading: Soft Slate Blue (#565f89)
+              (propertize (concat num-str " │ ") 
+                          'face '(:foreground "#565f89"))
+            ;; Subheadings: Deep Slate (#4a506a)
+            (propertize (concat num-str " — ") 
+                        'face '(:foreground "#4a506a")))))))
+
+;; (use-package org-superstar
+;;   :ensure t
+;;   :hook (org-mode . org-superstar-mode)
+;;   :config
+;;   (setq org-superstar-headline-bullets-list '(""))
+;;   (setq org-superstar-leading-bullet ""))
 
 (straight-use-package '(nano-emacs :type git :host github :repo "rougier/nano-emacs"))
 (require 'nano-writer)
